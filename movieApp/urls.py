c283dc8df3fe6489daaf9f8c5834e233ff17e687
stan05 <stanislav.petr7@gmail.com>
2019-07-04 21:23:38 +0100

    added back end for add new  movie function

--- movieApp/urls.py
+++ movieApp/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    
+    path('create/', views.movie_create, name='movie_create'),
 ]
 
--- movieApp/views.py
+++ movieApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 import os
 from airtable import Airtable
@@ -15,19 +15,30 @@ AT = Airtable(AIRTABLE_BASE_ID, TABLE_NAME)
 # Create your views here.
 
 def home_page(request):
-    user_query = str(request.GET.get('query', ''))
-    # pulls information from our query (index.html)    print(user_query)
-    search_result = AT.get_all(formula="FIND('" + user_query.lower() + "',LOWER({Name}))")
-    stuff_for_frontend = {'search_result':search_result}
-    return render(request, 'movieApp/index.html', stuff_for_frontend)
+        user_query = str(request.GET.get('query', ''))
+        # pulls information from our query (index.html)    print(user_query)
+        search_result = AT.get_all(formula="FIND('" + user_query.lower() + "',LOWER({Name}))")
+        stuff_for_frontend = {'search_result':search_result}
+        return render(request, 'movieApp/index.html', stuff_for_frontend)
 
-    """
-    for i in range(len(database)):
-            try:                if user_query.lower() in str(database[i]['fields']['Name']).lower():
-                        search_result.append(str(database[i]['fields']))
-            excEpt KeyError:
-                    pass
-    
-    return render(request, 'movieApp/index.html')
+def movie_create(request):
+        if request.method == 'POST':
+                new_movie = {
+                        'Name': request.POST.get('Name'),
+                        'Notes': request.POST.get('Notes'),
+                        'Pictures': [{'url': request.POST.get('Pictures')}],
+                        'Rating': int(request.POST.get('Rating'))
+                }
+                AT.insert(new_movie)
+        return redirect('/')
 
-    """
+"""
+for i in range(len(database)):
+        try:                if user_query.lower() in str(database[i]['fields']['Name']).lower():
+                search_result.append(str(database[i]['fields']))
+        excEpt KeyError:
+                pass
+
+return render(request, 'movieApp/index.html')
+
+"""
