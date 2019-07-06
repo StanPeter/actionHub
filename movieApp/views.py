a81122fcc20571c8204e04fb047968ca9be05a8d
stan05 <stanislav.petr7@gmail.com>
2019-07-06 22:55:30 +0100

    styling changed

--- movieApp/views.py
+++ movieApp/views.py
@@ -15,22 +15,24 @@ AT = Airtable(AIRTABLE_BASE_ID, TABLE_NAME)
 # Create your views here.
 
 def home_page(request):
-        user_query = str(request.GET.get('query', ''))
-        # pulls information from our query (index.html)    print(user_query)
-        search_result = AT.get_all(formula="FIND('" + user_query.lower() + "',LOWER({Name}))")
-        stuff_for_frontend = {'search_result':search_result}
-        return render(request, 'movieApp/index.html', stuff_for_frontend)
+    user_query = str(request.GET.get('query', ''))
+    # pulls information from our query (index.html)    print(user_query)
+    search_result = AT.get_all(formula="FIND('" + user_query.lower() + "',LOWER({Name}))")
+    stuff_for_frontend = {'search_result':search_result}
+    return render(request, 'movieApp/index.html', stuff_for_frontend)
 
 def movie_create(request):
-        if request.method == 'POST':
-                new_movie = {
-                        'Name': request.POST.get('Name'),
-                        'Notes': request.POST.get('Notes'),
-                        'Pictures': [{'url': request.POST.get('Pictures')}],
-                        'Rating': int(request.POST.get('Rating'))
+    if request.method == 'POST':
+        new_movie = {
+            'Name': request.POST.get('Name'),
+            'Notes': request.POST.get('Notes'),
+            'Pictures': [{'url': request.POST.get('Pictures')}],
+            'Rating': int(request.POST.get('Rating'))
                 }
-                AT.insert(new_movie)
-        return redirect('/')
+        AT.insert(new_movie)
+    return redirect('/')
+
+
 
 """
 for i in range(len(database)):
